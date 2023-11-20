54c16856f459edce732fecf80dbfbb24f50a31fc
ghostchinchilla <beca.mckay@yahoo.com>
2023-11-19 20:15:24 -0500

    added second query

--- joins-exercises/queries.sql
+++ joins-exercises/queries.sql
@@ -1,3 +1,9 @@
 SELECT * FROM owners o 
   FULL OUTER JOIN vehicles v 
     ON o.id=v.owner_id;
+
+SELECT first_name, last_name, 
+  COUNT(owner_id) FROM owners o 
+  JOIN vehicles v on o.id=v.owner_id 
+  GROUP BY (first_name, last_name) 
+ORDER BY first_name;
